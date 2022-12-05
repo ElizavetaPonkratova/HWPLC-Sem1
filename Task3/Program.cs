2a85e36e88b86607b6550e038530596350d8925b
ElizavetaPonkratova <elizaveta.ski@yandex.ru>
2022-12-05 18:49:14 +0700

    Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Например: 4 -> да -3 -> нет 7 -> нет*/
+
+Console.WriteLine("Enter number:");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 1)
+{
+      Console.WriteLine(num + " NO");
+}
+else
+{
+       Console.WriteLine(num + " YES");
+}
